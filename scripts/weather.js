755db403a9d89cfb27007448ae3d22866f637aa7
kalinabahchevanova <k.bahchevanova95@gmail.com>
2016-05-14 16:27:33 +0300

    Get real data from public weather api

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-	$("#temp").text("15");
-	$("#city").text("Sofia");
-	$("#weather").text("Rainy");
-	var days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-	var now = new Date();
-	var day = days[ now.getDay() ];
-	$("#date").text(day);
+	$("#searchButton").click(function(){
+		var searchCity=$("#searchCity").val();
+		var api="http://api.openweathermap.org/data/2.5/weather?q="+searchCity+"&units=metric&appid=effc043e317bfe1280e0d6f0b72fa509";
+		$.getJSON( api, function(data){
+			$("#temp").text(data.main.temp);
+			$("#weather").text(data.weather[0].main);
+		});
+		var days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+		var now = new Date();
+		var day = days[ now.getDay() ];
+		$("#date").text(day);
+	});
+	
 });
